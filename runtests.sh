ee6d535298cbea7e4f50db7bddbfe5fbdfce7be0
camercu <unterbergerc@live.com>
2018-10-20 11:52:02 -0500

    add example test script

---
+++ runtests.sh
@@ -0,0 +1,29 @@
+##########################################################
+# Example test script to run unit tests (see minunit.h)
+# Assumes test binaries are in a "tests/" subfolder
+# From Zed Shaw's "Learn C the Hard Way"
+# url: http://c.learncodethehardway.org/book/ex28.html
+##########################################################
+
+ATTR_BOLD=$(tput bold)
+ATTR_RESET=$(tput sgr0)
+COLOR_RED=$(tput setaf 1)
+
+echo "${ATTR_BOLD}>>> Running unit tests:${ATTR_RESET}"
+
+for i in tests/*_tests; do
+	if [ -f "$i" ]; then
+		VALGRIND=
+		# VALGRIND="valgrind --leak-check=full --log-file=tests/valgrind-${i}.log"
+		if $VALGRIND "./$i" 2>> tests/tests.log; then
+			echo "${ATTR_BOLD}+++ $i PASS${ATTR_RESET}"
+		else
+			echo "${COLOR_RED}${ATTR_BOLD}>>> ERROR in test '$i':${ATTR_RESET} here's the tests/tests.log"
+			echo "--------------"
+			tail tests/tests.log
+			exit 1
+		fi
+	fi
+done
+
+echo
