1ed57043dfc968c3b2cc2ef539b69120d122de79
camercu <cameron.unterberger@gmail.com>
2019-07-26 23:09:06 -0400

    reformat

--- utils.h
+++ utils.h
@@ -15,22 +15,19 @@ extern "C" {
 #define ABS(VALUE) ((VALUE) < 0 ? -(VALUE) : (VALUE))
 #define MIN(A, B) ((A) < (B) ? (A) : (B))
 #define MAX(A, B) ((A) > (B) ? (A) : (B))
-#define SWAP(A, B) do { (A) ^= (B); (B) ^= (A); (A) ^= (B); } while (0)
 
 /* Swaps simple integral datatypes around (e.g. integers, pointers) */
-#define SWAP(A, B)       \
-    do {                 \
-        (A) = (A) ^ (B); \
-        (B) = (B) ^ (A); \
-        (A) = (A) ^ (B); \
+#define SWAP(A, B)  \
+    do {            \
+        (A) ^= (B); \
+        (B) ^= (A); \
+        (A) ^= (B); \
     } while (0)
 
 /* macro to restart a system call that was interrupted by a signal.
    Source: Kerrisk, Michael. The Linux Programming Interface: A Linux and UNIX
    System Programming Handbook. No Starch Press. San Francisco, 2010. pg. 443 */
-#define NO_EINTR(STMT)                     \
-    while ((STMT) == -1 && errno == EINTR) \
-        ;
+#define NO_EINTR(STMT) while ((STMT) == -1 && errno == EINTR);
 /* example usage:
    NO_EINTR(cnt = read(fd, buf, BUF_SIZE));
    check(cnt != -1, "read() failed with other than EINTR"); */
