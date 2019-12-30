b6408df939265642ce0008cb7f0cebc68417ea61
Cameron Unterberger <camercu@users.noreply.github.com>
2019-12-30 09:06:48 -0500

    Add BOOLSTR()

--- include/utils.h
+++ include/utils.h
@@ -11,6 +11,20 @@
 #include <stdint.h>
 
 /**
+ * @brief Macro to replace a boolean expression with it's string equivalent
+ *
+ * This is useful for printf statements where you want to see the boolean displayed as a string.
+ *
+ * Example usage:
+ * @code
+ * bool is_false = 0;
+ * printf("Value of is_false: %s\n", BOOLSTR(is_false));
+ * @endcode
+ * This will print: <tt>Value of is_false: False</tt>
+ */
+#define BOOLSTR(BOOL_EXPR) ((BOOL_EXPR) ? "True" : "False")
+
+/**
  * @brief Return the absolute value of a numeric type
  */
 #define ABS(VALUE) ((VALUE) < 0 ? -(VALUE) : (VALUE))
@@ -32,25 +46,25 @@
 #define MAX(A, B) ((A) > (B) ? (A) : (B))
 
 /**
- * @brief Round @c x up to the nearest multiple of @c y.
+ * @brief Round @c X up to the nearest multiple of @c Y.
  * @warning
- * The numbers @c x and @c y must be of the same type
+ * The numbers @c X and @c Y must be of the same type
  */
-#define ROUNDUP(x, y) (((x) + (y) - 1) / (y) * (y))
+#define ROUNDUP(X, Y) (((X) + (Y) - 1) / (Y) * (Y))
 
 /**
- * @brief Round @c x down to the nearest multiple of @c y.
+ * @brief Round @c X down to the nearest multiple of @c Y.
  * @warning
- * The numbers @c x and @c y must be of the same type
+ * The numbers @c X and @c Y must be of the same type
  */
-#define ROUNDDOWN(x, y) ((x) / (y) * (y))
+#define ROUNDDOWN(X, Y) ((X) / (Y) * (Y))
 
 /**
- * @brief Get the ceiling of <tt>x / y</tt>
+ * @brief Get the ceiling of <tt>X / Y</tt>
  * @warning
- * The numbers @c x and @c y must be of the same type
+ * The numbers @c X and @c Y must be of the same type
  */
-#define CEILING(x, y) (((x) + (y) - 1) / (y))
+#define CEILING(X, Y) (((X) + (Y) - 1) / (Y))
 
 /**
  * @brief Swap the values contained by two numeric variables in place.
@@ -69,36 +83,36 @@
 /**
  * @brief Return the number of elements in an array
  */
-#define ARRAYLEN(arr) (sizeof((arr)) / sizeof(*(arr)))
+#define ARRAYLEN(ARR) (sizeof((ARR)) / sizeof(*(ARR)))
 
 /**
- * @brief Make an n-bit mask.
+ * @brief Make an N-bit mask.
  *
  * @warning
- * @c n must be in the range 1-31 (inclusive)!
+ * @c N must be in the range 1-31 (inclusive)!
  */
-#define BITMASK(n) ((1 << (n)) - 1)
+#define BITMASK(N) ((1 << (N)) - 1)
 
 /**
- * @brief Make a bitmask where the bits between indeces [low..hi] are set.
+ * @brief Make a bitmask where the bits between indeces [LOW..HI] are set.
  * @note
  * Index 0 is the least-significant bit
  * @warning
- * Caller must ensure <tt>low < high</tt>. @c low and @c hi must both
+ * Caller must ensure <tt>LOW < HIGH</tt>. @c LOW and @c HI must both
  * be in the range of [0..31].
  */
-#define MASKRANGE(low, hi) (((hi) - (low) + 1) << (low))
+#define MASKRANGE(LOW, HI) (((HI) - (LOW) + 1) << (LOW))
 
 /**
  * @brief Token concatenation for macros
  *
  * The X* macros allow the C preprocessor to prescan/expand the tokens to their definitions.
  */
-#define MAKESTR(s) #s
-#define XSTR(s) MAKESTR(s)
-#define CONC(x, y) x##y
-#define XCONC(x, y) CONC(x, y)
-#define XXCONC(x, y) XCONC(x, y)
+#define MAKESTR(S) #S
+#define XSTR(S) MAKESTR(S)
+#define CONC(A, B) A##B
+#define XCONC(A, B) CONC(A, B)
+#define XXCONC(A, B) XCONC(A, B)
 
 
 /**
@@ -141,11 +155,11 @@ static const union {
  * @brief Inform compiler of commonly expected value for branch prediction.
  */
 #if (__GNUC__ >= 3)
-#define LIKELY(expr) __builtin_expect(!!(expr), 1)
-#define UNLIKELY(expr) __builtin_expect((expr), 0)
+#define LIKELY(EXPR) __builtin_expect(!!(EXPR), 1)
+#define UNLIKELY(EXPR) __builtin_expect((EXPR), 0)
 #else
-#define LIKELY(expr) (expr)
-#define UNLIKELY(expr) (expr)
+#define LIKELY(EXPR) (EXPR)
+#define UNLIKELY(EXPR) (EXPR)
 #endif /* (__GNUC__ >= 3) */
 
 
@@ -153,9 +167,9 @@ static const union {
  * @brief Specify the minimum alignment in bytes (e.g. for structs)
  */
 #ifdef __GNUC__
-#define ALIGNED(n) __attribute__((__aligned__(n)))
+#define ALIGNED(N) __attribute__((__aligned__(N)))
 #else
-#define ALIGNED(n)
+#define ALIGNED(N)
 #endif /* __GNUC__
 
 
@@ -170,17 +184,17 @@ static const union {
 
 
 /**
- * @brief Perform code only once, even after successive calls to containing function
+ * @brief Perform CODE only once, even after successive calls to containing function
  *
  * @warning
  * This is not intended to be thread-safe!
  */
-#define DO_ONCE(code) \
+#define DO_ONCE(CODE) \
     do { \
         static bool _was_done_already = false; \
         if (UNLIKELY(!_was_done_already)) { \
             _was_done_already = true; \
-            code; \
+            CODE; \
         } \
     } while (0)
 
